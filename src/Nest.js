3b5a0a02ae858ebe851b2a0ffc64654a43e47cb2
adventurebear <suzanneatkinson@gmail.com>
2017-01-29 15:05:49 -0500

    refactored for commander .command usage

---
+++ src/Nest.js
@@ -0,0 +1,16 @@
+var React = require('react')
+
+var Nest = React.createClass({
+    render: function(){
+        return (
+            <div >
+                <h1>Nest</h1>
+            </div>
+        )
+    },
+    propTypes: {
+
+    }
+})
+
+module.exports = Nest
--- trot.js
+++ trot.js
@@ -15,81 +15,103 @@ let cssFilename = ""
 let dir = './'
 let outputName = ""
 
+
+program
+  .command('nest <parent> [otherFiles...]')
+  .description('nests child components into parent')
+  .option("-h, --children [children...]", "Child components to nest")
+  .action(function(parent, otherFiles){
+    console.log('Nest components  for %s parent and %s children', parent, otherFiles);
+  });
+
+
 //console.log(reactComponentEs5, typeof(reactComponentEs5))
 /*  Create Options list from command lines */
 program
   .version('0.0.1')
+  .command('comp')
+  .description('create components')
   .option('-c, --componentName [name]', 'Create Component Named [Layout]', 'layout')
   .option('-v, --JSVersion [version]', 'EMCAScript version [6]',  '6')
   .option('-f, --folder [folder]', 'Folder [./]',  './')
   .option('-s, --cssFile [cssFile]', 'Create CSS [No]', 'No')
-  .parse(process.argv);
+  .action(function(componentName, options){
+    console.log("Composing components...")
+    //if component flag set (also should be default)
+    if (componentName) {
+      console.log('Creating React component: ')
+      console.log('  - Component Name: %s ', this.componentName)
+      console.log('  - EMCAScript Version: %s ', this.JSVersion)
+      console.log('  - Folder: %s ', this.folder)
+      console.log('  - CSS file created: %s ', this.cssFile)
 
-//if component flag set (also should be default)
-if (program.componentName) {
-  console.log('Creating React component: ')
-  console.log('  - Component Name: %s ', program.componentName)
-  console.log('  - EMCAScript Version: %s ', program.JSVersion)
-  console.log('  - Folder: %s ', program.folder)
-  console.log('  - CSS file created: %s ', program.cssFile)
+      // Handle Version Flag "-v"
+      //select EMCA version 6 as default or version 5 if flagged
+      if (program.JSVersion==='6')
+        template = reactComponentEs6
+      else  template = reactComponentEs5
 
-  // Handle Version Flag "-v"
-  //select EMCA version 6 as default or version 5 if flagged
-  if (program.JSVersion==='6')
-    template = reactComponentEs6
-  else  template = reactComponentEs5
+      //Create component file from template, replacing holder text with componentName
+      //basic replacment regardless of CSS flag
+      result = template.replace(/\[comp\]/g, this.componentName)
 
-  //Create component file from template, replacing holder text with componentName
-  //basic replacment regardless of CSS flag
-  result = template.replace(/\[comp\]/g, program.componentName)
-
-  //Handle CSS flag (-s)
-  //Import CSS, create CSS from Template and add className to Div of component file
-  if (program.cssFile==='y') {
-    result = result.replace(/\[import-css-file\]/g, ("import \'.\/" + program.componentName + ".css\'" ))
-    result = result.replace(/\[create-css-class\]/g, ("className=\'component-" + program.componentName.toLowerCase() + "\'" ))
-    cssResult =cssTemplate.replace (/\[comp\]/g, program.componentName.toLowerCase())
-  }  else {
-    //remove CSS references from template
-    //result = template.replace(/\[comp\]/g, program.componentName)
-    result = result.replace(/\[import-css-file\]/g, '')
-    result = result.replace(/\[create-css-class\]/g, '')
-  }
-
-  //Check for -f directory and create if doesn't exist
-  if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
-  }
+      //Handle CSS flag (-s)
+      //Import CSS, create CSS from Template and add className to Div of component file
+      if (this.cssFile==='y') {
+        result = result.replace(/\[import-css-file\]/g, ("import \'.\/" + this.componentName + ".css\'" ))
+        result = result.replace(/\[create-css-class\]/g, ("className=\'component-" + this.componentName.toLowerCase() + "\'" ))
+        cssResult =cssTemplate.replace (/\[comp\]/g, this.componentName.toLowerCase())
+      }  else {
+        //remove CSS references from template
+        //result = template.replace(/\[comp\]/g, this.componentName)
+        result = result.replace(/\[import-css-file\]/g, '')
+        result = result.replace(/\[create-css-class\]/g, '')
+      }
 
 
-  //build strings for directory and file outputs
-  if (program.folder) {
-    dir += program.folder + '/'
-    cssFilename = dir + program.componentName + ".css"
-  }
-
-  outputName = dir + program.componentName + '.js'
+      //build strings for directory and file outputs
+      if (this.folder) {
+        dir += this.folder + '/'
+        cssFilename = dir + this.componentName + ".css"
+      }
 
 
-  //create component file in proper directory
-  fs.writeFile(outputName, result, 'utf8', function (err) {
-    if (err) return console.log(err)
-      console.log("New file created: ", outputName)
-  });
+      //Check for -f directory and create if doesn't exist
+      if (!fs.existsSync(dir)){
+        console.log("Directory does not exist: " + dir)
+        fs.mkdirSync(dir);
+      }else {
+        console.log("Directory exists: " + dir)
+      }
 
-  //create CSS file in proper directory
-  if (program.cssFile==='y') {
-    fs.writeFile(cssFilename, cssResult, 'utf8', function (err) {
-      if (err) return console.log(err)
-      console.log("New CSS file created: ", cssFilename)
-    });
-  }
-}
+      //create component file in proper directory
+      outputName = dir + this.componentName + '.js'
+      fs.writeFile(outputName, result, 'utf8', function (err) {
+        if (err) return console.log(err)
+        console.log("New file created: ", outputName)
+      });
+
+      //create CSS file in proper directory
+      if (this.cssFile==='y') {
+        fs.writeFile(cssFilename, cssResult, 'utf8', function (err) {
+          if (err) return console.log(err)
+          console.log("New CSS file created: ", cssFilename)
+        });
+      }
+    }
+})
 
 
 
+
+/* Command for nesting child components
+   let's see how it works.
+ */
+
+program.parse(process.argv);
 
 //TODO-DONE:  add a flag for directory
 //TODO-DONE:  add error checking if directory does not exist
 //TODO-DONE: make it a shell script (run npm install -g) to remake
-//TODO:  Add css flag to auto create CSS files
+//TODO-DONE:  Add css flag to auto create CSS files
+//TODO:  Add command to create nesting & import needed components.
