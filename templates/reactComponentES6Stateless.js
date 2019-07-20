0962a153c20054febf400c8bc4b4b3d5111de11e
Abhishek Venkat <abhisaivenkat@gmail.com>
2019-07-20 14:32:23 -0400

    Allow stateless/function component

---
+++ templates/reactComponentES6Stateless.js
@@ -0,0 +1,27 @@
+/* ES6 Syntax */
+
+var ES6 = 
+
+`import React, { Component } from 'react'
+import PropTypes from 'prop-types' //ES6
+
+
+[import-css-file]
+
+const [comp] = (props) => {
+
+    return (
+        <div [create-css-class]>
+            <h1>[comp]</h1>
+        </div>
+    )
+
+}
+
+[comp].propTypes = {
+
+};
+
+export default [comp]`
+
+module.exports = ES6
